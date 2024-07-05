7667a4e940795f9433f87a7b6f5e550d36b8b63a
zhaoleiaa <zhao13783345951lei>
2024-07-05 10:01:27 +0800

    fix: 更新为云服务器mysql

--- packages/pet-walk-park-service/data-source.ts
+++ packages/pet-walk-park-service/data-source.ts
@@ -3,11 +3,11 @@ import { DataSource } from 'typeorm'
 
 const dataSource = new DataSource({
   type: 'mysql', // 数据库类型
-  host: 'localhost', // mysql 服务地址
-  port: 3306, // mysql 服务启动端口
-  username: 'root', // mysql 用户名
-  password: 'admin0125', // mysql 密码
-  database: 'zimuadmin', // 数据库
+  host: '116.198.247.204', // 云服务器的 IP 地址
+  port: 3306, // MySQL 服务启动端口
+  username: 'root', // MySQL 用户名
+  password: 'Z931125&l', // MySQL 密码
+  database: 'programdb', // 数据库名称
   entities: [path.join(__dirname, '/../**/*.entity.{js,ts}')], // typeorm 实体
   entityPrefix: 'zm-', // 数据库表前缀
   logging: true // 开启日志
